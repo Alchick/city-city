482216c28e007c786177bf210ddd5803319bb150
E.Sergeev <e.sergeev@i-free.com>
2016-08-06 21:59:18 +0300

    bad new about forms

--- app/forms.py
+++ app/forms.py
@@ -1,26 +1,48 @@
 #coding: utf-8
-from flask_wtf import Form
+from flask_wtf import Form, PasswordField, SubmitField
 from wtforms import TextField
-#from wtforms.validators import Length, Email, Regexp, DataRequired, Required, ValidationError
-from wtforms.validators import *
+from wtforms.validators import Length, Email, Regexp, DataRequired, Required, ValidationError
 from werkzeug.security import generate_password_hash, check_password_hash
 import re
 
-
-class MainForm(Form):
-    author_name = TextField('author_name', description = 'author_name', validators=[Required(message = u'Введите Имя'),\
-                                                                             Regexp(r'^[a-zA-Z]+$', message = u'Неверный символ'),\
+class CreateForm(Form):
+    reg = re.compile(u'^[а-яА-Я ]+$')
+    #reg = re.compile(u'^[a-zA-Z]+$')
+    author_name = TextField('author_name', validators=[Required(message = u'Введите Имя'),\
+                                                                             Regexp(reg, message = u'Неверный символ'),\
                                                                              Length(max=40)])
-    article_name = TextField('article_name', description = 'article_name', validators=[required(message = u'введите название статьи'),\
-                                                                             regexp(r'^[a-za-z0-9]+$', message = u'неверный символ'),\
-                                                                             length(max=40)])
-    email = TextField('email', validators = [Required(message = u'Введит электронный адрес')])
-                                             #Email(message = u'Неверный почта')])
-    comment = TextField('comment', description = 'comment', validators=[required(message = u'Введите комментарий'),\
-                                                                             regexp(r'^[a-za-z0-9]+$', message = u'неверный символ'),\
-                                                                             length(max=500)])
+    article_name = TextField('article_name', validators=[Required(message = u'введите название статьи'),\
+                                                                             Regexp(reg, message = u'Неверный символ'),\
+                                                                             Length(max=40)])
+    email = TextField('email', validators = [Required(message = u'Введит электронный адрес'),\
+                                            Email(message = u'Неверный почта')])
+    submit = SubmitField('submit')
 
 class LoginForm(Form):
-    login = TextField('login', validators=[Required(message=u'Пустое поле')]) #Test Regexp
-    password = TextField('password', validators=[Required(message=u'Пустое поле')]) #Test regexp
+    reg = re.compile(u'^[а-яА-Я ]+$')
+    #reg = re.compile(u'^[a-zA-Z]+$')
+    login = TextField('login', validators=[Required(message=u'Пустое поле')])
+    password = PasswordField('password', validators=[Required(message=u'Пустое поле')])
 
+class FindForm(Form):
+    reg = re.compile(u'^[а-яА-Я ]+$')
+    #reg = re.compile(u'^[a-zA-Z]+$')
+    author_name = TextField('author_name', validators=[Required(message = u'Введите Имя'),\
+                                                                Regexp(reg, message = u'Неверный символ'),\
+                                                                Length(max=40)])
+    article_name = TextField('article_name',validators=[Required(message = u'введите название статьи'),\
+                                                                Regexp(reg, message = u'неверный символ'),\
+                                                                Length(max=40)])
+
+class CommentForm(Form):
+    reg = re.compile(u'^[а-яА-Я ]+$')
+    #reg = re.compile(u'^[a-zA-Z]+$')
+    author_name = TextField('author_name', validators=[Required(message = u'Введите Имя'),\
+                                                       Regexp(reg, message = u'Неверный символ'),\
+                                                       Length(max=40)])
+    email = TextField('email', validators = [Required(message = u'Введит электронный адрес'),\
+                                            Email(message = u'Неверный почта')])
+
+    comment = TextField('comment', validators=[Required(message = u'Введите комментарий'),\
+                                               Regexp(reg, message = u'Неверный символ'),\
+                                               Length(max=400)])
