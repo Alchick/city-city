1a6c62248cae51fccc68a61035dff2202ef83d73
E.Sergeev <e.sergeev@i-free.com>
2016-08-27 09:38:27 +0300

    correct config

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 #coding=utf-8
 import os
 MAX_CONTENT_LENGTH = 100 * 1024 * 1024
-USE_X_SENDFILE = True #test how to send cache files to browser
-SERVER_NAME = 'culture-city.rf'
-SEND_FILE_MAX_AGE_DEFAULT = '43200'
+#USE_X_SENDFILE = True #test how to send cache files to browser
+#SERVER_NAME = 'culture-city.rf'
+#SEND_FILE_MAX_AGE_DEFAULT = '43200'
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 #check upload folder before start
-UPLOAD_FOLDER = BASEDIR + '/static/filestorage/'
+UPLOAD_FOLDER = BASEDIR + '/app/static/filestorage/'
 ALLOWED_EXTENSIONS = set(['pdf', 'jpeg', 'jpg','doc', 'docx', 'txt'])
 CSRF_ENABLED = True
 SECRET_KEY = 'Ooshaev6ie8eeRok'
@@ -20,21 +20,20 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:5432/{3}'.format(\
                                                               DATABASE_USER,\
                                                               DATABASE_PASSWORD,\
                                                               DATABASE_HOST,\
-                                                              DATABASE_PASSWORD)
-SQLALCHEMY_BINDS = {
-    'culture_city-mysql':'mysql://{0}:{1}@{2}:5432/{3}'.format(\
-                                                        DATABASE_USER,\
-                                                        DATABASE_PASSWORD,\
-                                                        DATABASE_HOST,\
-                                                        DATABASE_PASSWORD)
-    }   
+                                                              DATABASE_NAME)
+#SQLALCHEMY_BINDS = {
+#    'culture_city-mysql':'mysql://{0}:{1}@{2}:5432/{3}'.format(\
+#                                                        DATABASE_USER,\
+#                                                        DATABASE_PASSWORD,\
+#                                                        DATABASE_HOST,\
+#                                                        DATABASE_PASSWORD)
+#    }   
 #EMAIL CONFIGURATION
-MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_SERVER = 'smtp.mail.ru'
 MAIL_PORT = 25
 MAIL_USE_TLS = True
-MAIL_USE_SSL = True
-MAIL_USERNAME = 'nano.e.t.4'
-MAIL_PASSWORD = 'dont remember'
+MAIL_USERNAME = 'garfield04@mail.ru'
+MAIL_PASSWORD = 'Mi8aircraft'
 
 
 
