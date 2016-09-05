c4d520f1c531cd1067eb0ed7fccce90f74288ec3
root <garfield04@mail.ru>
2016-09-05 21:13:01 +0300

    add message fadeOut function

--- app/static/javascript/tiny_scripts.js
+++ app/static/javascript/tiny_scripts.js
@@ -1,4 +1,4 @@
-function sendPost(form) {
+function send_comment(form, person) {
     var date = $(form).serialize();
     val = document.getElementById('counter');
     if (val){
@@ -8,31 +8,71 @@ function sendPost(form) {
         type:'post',//тип запроса: get,post либо head
         url:'set_comment.html',//url адрес файла обработчика
         data:date,//параметры запроса
-        v:val,
         response:'success',//тип возвращаемого ответа text либо xml
-        success:function(data,v){//возвращаемый результат от сервера
-            if (val){
-                alert(val);
+        success:function(data){//возвращаемый результат от сервера
                 var message = JSON.parse(JSON.stringify(data));
+                $('#result').css('color',message['color']);
                 $('#result').html(message['message_words']);
-              }
-            else {
-              Data = new Date();
-              var new_comment = "<p>Имя пользователя - " + $("input[name=name]").val()+ "</p>"+
-                                "<p>Дата добавления - " + moment(Data).fromNow() + "</p>" +
-                                "<p>Комментарий -  " + $("textarea[name=comment]").val()+"</p>";
-              $('#new_comment').html(new_comment);
-            }
-        },
+                $('#result').fadeOut(2000);
+              },
         error:function(){
-            message = "<p style=\"color:red\">Произошла ошибка при отправке данных. Попробуйте снова или напишите нам</p>";
+            message = "<p>Произошла ошибка при отправке данных. Попробуйте снова или напишите нам</p>";
+            $('#result').css('color','red');
             $('#result').html(message);
+            $('#result').fadeOut(2000);
         }
     });
 }
 
+function add_comment(data, person){
+//     var comments = document.getElementById(object_id);
+     var comments_place = document.getElementById(person+'_comments');
+     comments_place.innerHTML = '';
+     for (var i = 0; i<data.length; i++){
+        var comment = document.createElement('div');
+        comment.className = 'comment';
+        if (person == 'user') {
+            comment.innerHTML = "<p><span class='comment_field'>Имя пользователя</span>: " + data[i][0] + "</p>" +
+        "<p><span class='comment_field'>Комментарий</span>: " + data[i][2] + "</p>"+
+        "<p><span class='comment_field'>Дата добавления</span>: " + moment(data[i][1]).fromNow() + "</p><br>";
+        }
+        else{
+            comment.innerHTML = "<p><span class='comment_field'>Имя пользователя</span>: " + data[i][3] + "</p>" +
+        "<p><span class='comment_field'>Комментарий</span>: " + data[i][2] + "</p>"+
+        "<p><span class='comment_field'>Дата добавления</span>: " + moment(data[i][1]).fromNow() + "</p>" + 
+        "<p><span class='comment_field'>Рейтинг</span>: " + data[i][0] + "</p><br>";
+        }
+        comments_place.appendChild(comment);
+     }
+}
 
-setTimeout(function(){$('.flash-message').fadeOut('fast')}, 5000);
+function get_comment(article_id, current_iter, person){
+    i = i + current_iter;
+    var next_page = document.querySelector('#'+person+'_next_button');
+    var previous_page = document.querySelector('#'+person+'_previous_button');
+    $.ajax({    
+    type:'GET',
+    url:'get_comment',
+    data:{id:article_id,iter:i, person:person},
+    success:function(data){
+        add_comment(data[person+'_comments'], person);
+        if (data[person+'_length'] <= 3){
+            next_page.style.display = "none"}
+
+        else{next_page.style.display = "inline"}
+
+        if (i < 2){
+            previous_page.style.display = 'none'}
+            //document.getElementById('previous_page').style.display = 'none'}
+            else{previous_page.style.display = 'inline'};
+        
+    }
+   });
+}
+
+    
+setTimeout(function(){
+    $('.flash-message').fadeOut('fast')}, 5000);
 
 function doClear(element) {
     if (element.value == element.defaultValue) { element.value = "" };
@@ -62,46 +102,3 @@ function time_format(data){
 function time_fromNow(data){
     document.write(moment.utc(data).local().fromNow());
 }
-
-function add_comment(data, person){
-//     var comments = document.getElementById(object_id);
-     var comments = document.getElementById(person+'_comments');
-     comments.innerHTML = '';
-     for (var i = 0; i<data.length; i++){
-        var comment = document.createElement('div');
-        comment.className = 'comment';
-        console.log(data[i][0]);
-        comment.innerHTML = "<p><span class='comment_field'>Имя пользователя</span> - " + data[i][0] + "</p>" +
-        "<p><span class='comment_field'>Комментарий</span> - " + data[i][2] + "</p>"+
-        "<p><span class='comment_field'>Дата добавления</span> - " + moment(data[i][1]).fromNow() + "</p><br>";
-        comments.appendChild(comment);
-     }
-}
-
-
-function get_comment(article_id, j, person){
-    i = i + j;
-    var next_page = document.querySelector('#'+person+'_next_button');
-    var previous_page = document.querySelector('#'+person+'_previous_button');
-    console.log('this is '+ next_page);
-    $.ajax({    
-    type:'GET',
-    url:'get_comment',
-    data:{id:article_id,iter:i, person:person},
-    success:function(data){
-        add_comment(data[person+'_comments'], person);
-        if (data[person+'_length'] <= 3){
-            next_page.style.display = "none"}
-
-        else{next_page.style.display = "inline"}
-
-        if (i < 2){
-            previous_page.style.display = 'none'}
-            //document.getElementById('previous_page').style.display = 'none'}
-            else{previous_page.style.display = 'inline'};
-        
-    }
-   });
-}
-
-    
